f39c2d4eb972aeb32a736546f2b36b109594303a
Ludo <ludo.pradel@protonmail.com>
2026-09-10 12:14:01 +0200

    update affichage

--- src/main/java/iut/calculatrice.java
+++ src/main/java/iut/calculatrice.java
@@ -6,5 +6,6 @@ public class calculatrice
     public static void main( String[] args )
     {
         System.out.println( "Calculatrice" );
+        System.out.println( "Soustraction pas faite" );
     }
 }
