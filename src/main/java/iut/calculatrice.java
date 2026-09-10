7a3ebe371b7a1fcbfbe52d3d319f3a11d00f0254
Etudiant <Etudiant@INFO-B104-PC01.crie.i-univ-tlse2.fr>
2026-09-10 10:13:21 +0200

    calculatrice deux ligne

--- src/main/java/iut/calculatrice.java
+++ src/main/java/iut/calculatrice.java
@@ -5,6 +5,7 @@ public class calculatrice
 {
     public static void main( String[] args )
     {
+        System.out.println( "Calculatrice" );
         System.out.println( "Addition fonctionnelle dans la calculatrice" );
     }
 }
